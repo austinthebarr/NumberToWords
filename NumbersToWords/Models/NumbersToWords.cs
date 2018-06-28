29944bafad948f31478c9ee2aa19055a325e9661
Austin Barr and Alex Bunnell <austin@>
2018-06-28 11:56:53 -0700

    add tests

---
+++ NumbersToWords/Models/NumbersToWords.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Numbers2Words.Models
+  {
+    public class Numbers2Words
+    {
+      private int _userNumber;
+
+      public Numbers2Words(int theNumber)
+      {
+        _userNumber = theNumber;
+      }
+
+      
+    }
+}
